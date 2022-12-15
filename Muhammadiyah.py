184310526ec51eab0158b1d80b70733338cc78a1
Joko Supriyanto <jokosupriyanto1712@gmail.com>
2022-12-15 08:59:12 +0700

    Update UI

--- Muhammadiyah.py
+++ Muhammadiyah.py
@@ -1,7 +1,35 @@
 import tkinter as tk,urllib.request
+
 root = tk.Tk()
-with urllib.request.urlopen("https://id.wikipedia.org/wiki/Muhammadiyah") as response:
-    file = response.read()
-label = tk.Label(root, text=file)
+root.geometry("1280x720")
+root.title("Muhammadiyah")
+root.configure(bg = "#227C70")
+menu = tk.Menu(root)
+
+class barMenu():
+    def close():
+        exit()
+
+
+# home menubar
+home = tk.Menu(menu, tearoff=0)
+home.add_command(label="SignIn")
+home.add_command(label="Login")
+home.add_command(label="Exit",command=barMenu.close)
+menu.add_cascade(label="Home",menu=home)
+
+# settings menubar
+
+helpmenu = tk.Menu(menu, tearoff=0)
+helpmenu.add_command(label="Tips & Trick")
+helpmenu.add_command(label="Documentation")
+helpmenu.add_command(label="About")
+menu.add_cascade(label="help", menu=helpmenu)
+
+root.config(menu=menu)
+
+# with urllib.request.urlopen("https://id.wikipedia.org/wiki/Muhammadiyah") as response:
+#     file = response.read()
+label = tk.Label(root,text= "Selamat datang di Muhammadiyah",font =("Roboto",16),bg="#227C70")
 label.pack()
 root.mainloop()
