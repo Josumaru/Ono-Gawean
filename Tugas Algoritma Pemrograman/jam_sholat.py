3290465f38ee20137f44a738dfb0111aec312eaa
FadQode <fadhilerdyaqasmal@gmail.com>
2022-12-21 20:59:18 +0700

    menampilkan waktu tersisa sebelum sholat lagi

--- Tugas Algoritma Pemrograman/jam_sholat.py
+++ Tugas Algoritma Pemrograman/jam_sholat.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import pandas
 from tkinter import ttk
 from time import strftime
-
+from datetime import time
 
 my_font = ('times', 52, 'bold')  # display size and style
 
@@ -20,7 +20,7 @@ def shubuh(data=df, tanggal=tanggal):
     sholat = 'shubuh'
     jamsekarang(waktu_sholat, sholat)
 
-    
+
 def zhuhr(data=df, tanggal=tanggal):
     zhuhr_dict = {
         row.Tanggal:row.Zhuhr for (index, row) in df.iterrows()
@@ -55,19 +55,36 @@ def isya(data=df, tanggal=tanggal):
 
 def jamsekarang(waktu_sholat, sholat):
     my_w = tk.Tk()
-    my_w.geometry("405x300")
+    my_w.geometry("545x300")
     def my_time():
         time_string = strftime('%H:%M:%S %p')  # time format
         l1.config(text=time_string)
         l1.after(1000, my_time)  # time delay of 1000 milliseconds
-        
+    def selisih_waktu(waktu_sholat=waktu_sholat ):
+        jam = int(strftime('%H'))
+        menit = int(strftime('%M'))
+        jam_sholat = int(waktu_sholat[:2])
+        menit_sholat=int(waktu_sholat[4:6])
+
+        hours = jam_sholat - jam
+        if hours < 0:
+            hours = hours + 24
+        min = menit_sholat - menit
+        if min < 0:
+            min = min + 60
+        mine.config(text= f"masih ada {hours} jam dan {min} menit lagi sebelum sholat {sholat} kembali")
+
 
     my_font = ('times', 52, 'bold')  # display size and style
     l1 = tk.Label(my_w, font=my_font, bg='green')
     l1.grid(row=1, column=1, padx=5, pady=25)
-    waktu = tk.Label(my_w ,font=('times', 15, 'bold'), text=f"waktu sholat {sholat} hari ini jam {waktu_sholat}")
+    mine = tk.Label(my_w, font=('times', 15, 'bold'))
+    mine.grid(row=3, column=1, pady=10)
+    waktu = tk.Label(my_w ,font=('times', 15, 'bold'), text=f"waktu sholat {sholat} tanggal {tanggal} jam {waktu_sholat}")
     waktu.grid(row=2, column=1)
     my_time()
+    selisih_waktu()
     my_w.mainloop()
+
 
 
