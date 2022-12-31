b5747d99c79e71b864840393db389d652c7ba263
FadQode <fadhilerdyaqasmal@gmail.com>
2022-12-31 13:12:53 +0700

    menambah class

--- ono_gawean_site/search_engine/views.py
+++ ono_gawean_site/search_engine/views.py
@@ -5,12 +5,11 @@ import urllib,html2text,bs4,random
 def index(request):
     return render(request, "search_engine/index.html")
 
-def userInput(request):
-    if request.method == "POST":
-        search = request.POST["search"]
-
-        baseUrl = f"https://muhammadiyah.or.id/?s={search}"
-        requset = urllib.request.Request(baseUrl)
+class url_usage():
+    def __init__(self, url):
+        self.url = url
+    def url_choose(self):
+        requset = urllib.request.Request(self.url)
         response = urllib.request.urlopen(requset)
         resData = response.read()
         parsedhtml = bs4.BeautifulSoup(resData, "html.parser")
@@ -18,14 +17,29 @@ def userInput(request):
         randomNumber = random.randint(170,190)
         getUrl = (urlText[randomNumber])
         url = getUrl["href"]
+        return url
 
-        requset = urllib.request.Request(url)
+    def url_parse(self):
+        requset = urllib.request.Request(self.url)
         response = urllib.request.urlopen(requset)
         resData = response.read()
         parsedhtml = bs4.BeautifulSoup(resData, "html.parser")
         urlText = parsedhtml.findAll("p")
         tupleText = str(tuple(urlText))
         txt = html2text.html2text(tupleText)
+        return txt
+
+
+def userInput(request):
+    if request.method == "POST":
+        search = request.POST["search"]
+
+        baseUrl =url_usage(f"https://muhammadiyah.or.id/?s={search}")
+        url = baseUrl.url_choose()
+
+        chosen_url = url_usage(url)
+        txt = chosen_url.url_parse()
+
         return render (request, "search_engine/index.html",
         {"txt" : txt})
 
